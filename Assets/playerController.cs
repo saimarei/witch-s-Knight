91711d6a3c38c2003ea965e6b845c968d8879077
subbu <r.ssmsd@gmail.com>
2019-08-22 12:54:59 +0530

    got it to work but movement only works with moveposition and not velocity. idk why

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -4,5 +4,49 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private Animator playerAnimator;
+
+
+    void Start()
+    {
+        
+        playerAnimator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator.SetFloat("moveX", 0);
+        playerAnimator.SetFloat("moveY", -1);
+    }
+
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            change.x = Mathf.Round(change.x);
+            change.y = Mathf.Round(change.y);
+            playerAnimator.SetFloat("moveX", change.x);
+            playerAnimator.SetFloat("moveY", change.y);
+            playerAnimator.SetBool("moving", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        change.Normalize();
+        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
 }
