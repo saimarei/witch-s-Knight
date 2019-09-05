eaf80faa49a2489d3fec239e6ddfae23e7164186
subbu <r.ssmsd@gmail.com>
2019-09-06 00:22:10 +0530

    tried fixing wall collision, and implemented rolling

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,19 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;//required for using ui stuff duh.
 using TMPro; //required for using textmeshpro.
+using UnityEngine.EventSystems;
 public class playerController : MonoBehaviour
 {
-    public float speed;
+    public float speed=5f;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator playerAnimator;
     private TextMeshProUGUI test; //just for testing purposes will remove
-    public float dashSpeed;
+    public float dashSpeed=10f;
     public float dashTime;
-    public float dashStopTime;
-    private float currentTime;
+    private float startdashTime=1f;
+    private int taps = 0;
+    public Button top, bottom, left, right;
+    private float timer=0,maxWaitTime=0.5f;
+    private bool dashing = false;
+    private int direction;
 
     void Start()
     {
@@ -22,7 +28,13 @@ public class playerController : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody2D>();
         playerAnimator.SetFloat("moveX", 0);
         playerAnimator.SetFloat("moveY", -1);//sets it to -1 so it doesnt't assume any states
-        test = FindObjectOfType<TextMeshProUGUI>(); 
+        test = FindObjectOfType<TextMeshProUGUI>();
+        dashTime = startdashTime;
+        top.onClick.AddListener(CheckButtonClick);
+        bottom.onClick.AddListener(CheckButtonClick);
+        left.onClick.AddListener(CheckButtonClick);
+        right.onClick.AddListener(CheckButtonClick);
+
     }
 
     void Update()
@@ -30,20 +42,52 @@ public class playerController : MonoBehaviour
         change = Vector3.zero;
         change.x = SimpleInput.GetAxisRaw("Horizontal");
         change.y = SimpleInput.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
-        if (SimpleInput.GetButtonDown("A button"))
+        UpdateAnimationsAndMove();
+        if (SimpleInput.GetButtonDown("A button") || Input.GetKeyDown(KeyCode.Z))
         {
             test.text = "it's working A";//test button log
         }
-        else if(SimpleInput.GetButtonDown("B button"))
+        else if (SimpleInput.GetButtonDown("B button") || Input.GetKeyDown(KeyCode.X))
         {
-
             test.text = "it's working B";
         }
-
+        if (taps > 0)
+        {
+            timer += Time.deltaTime;
+            if (taps == 1)
+            {
+                if (timer >= maxWaitTime)
+                    taps = 0;
+            }
+            else if (taps > 1)
+            {
+                if (timer < maxWaitTime)
+                {
+                    dashing = true;
+                    dashSpeed = 10f;
+                }
+                else
+                {
+                    taps = 0;
+                    dashing = false;
+                }
+            }
+        }
+        else
+            timer = 0f;
+    }
+    private void FixedUpdate()
+    {
+        Dash();
     }
 
-    void UpdateAnimationAndMove()
+
+    void CheckButtonClick()
+    {
+        ++taps;
+    }
+
+    void UpdateAnimationsAndMove()
     {
         if (change != Vector3.zero)
         {
@@ -62,12 +106,45 @@ public class playerController : MonoBehaviour
 
     void MoveCharacter()
     {
-        change.Normalize();
-        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+        if (!dashing)
+        {
+            change.Normalize();
+            myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+        }
     }
 
     void Dash()
     {
-
+        try
+        {
+            dashTime -= Time.deltaTime;
+            if (dashing)
+            {
+                if (EventSystem.current.currentSelectedGameObject.name == "Top")
+                {
+                    myRigidbody.velocity = Vector2.up * dashSpeed; ;
+                }
+                else if (EventSystem.current.currentSelectedGameObject.name == "Right")
+                {
+                    myRigidbody.velocity = Vector2.right * dashSpeed;
+                }
+                else if (EventSystem.current.currentSelectedGameObject.name == "Bottom")
+                {
+                    myRigidbody.velocity = Vector2.down * dashSpeed;
+                }
+                else if (EventSystem.current.currentSelectedGameObject.name == "Left")
+                    myRigidbody.velocity = Vector2.left * dashSpeed;
+            }
+            else if (!dashing)
+            {
+                dashTime = startdashTime;
+                dashSpeed = 0f;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e, this);
+        }
     }
+    
 }
